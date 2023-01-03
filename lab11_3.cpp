96ae39d6dd1f0d8f2229e7bdb42bf196f197229a
aonnapaht <napaht_d@cmu.ac.th>
2023-01-03 16:43:28 +0700

    lab11_3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+string textline;
+float n=0;
+float sum=0;
+float u;
+float sig;
+float x=0;
+
+int main(){
+ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        x += pow(atof(textline.c_str()),2);
+        n++;
+    }
+u = sum/n;
+sig = sqrt(((1/n)*x) - pow(u,2));
+
+
+
+    cout << "Number of data = " << n << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << u << '\n';
+    cout << "Standard deviation = " << sig << '\n';
+}
